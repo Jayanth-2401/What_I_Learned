71d61bc76232dfb192f2f844a5b86fd94b37cab5
Jayanth H A <brahmacharyalife405@gmail.com>
2024-08-29 22:46:07 +0530

    First code

---
+++ C_C++_Dev/Basic_Code/01_main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void print_hello(void)
+{
+	cout << "Hello, Boisss...!" << endl;
+}
+
+// $ git branch --merged
+// * main
+// $ git log main..Basic_Code
+
+int main()
+{
+	print_hello();
+	return 0;
+}
